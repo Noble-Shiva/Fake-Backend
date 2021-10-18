eff3c27cbfa09dd6bb83d189dadeffa0394fc0ad
ShivaKumar Reddy L <l.shivkumarreddy@gmail.com>
2021-10-18 12:26:32 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log('This is a Fake Back-End Server');
+    res.send('This is a Fake Back-End Server')
+});
+
+app.post('/user', (req, res) => {
+    res.send(req.body);
+});
+
+app.listen(5000, () => {
+    console.log('App Listening at port 5000')
+});
