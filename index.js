84c93785dae9d8d4c1d8687f70300e753b1287a0
ShivaKumar Reddy L <l.shivkumarreddy@gmail.com>
2024-05-28 11:50:51 +0530

    Netlify Deployment Changes

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const serverless = require("serverless-http");
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log('This is a Fake Back-End Server');
-    res.send('This is a Fake Back-End Server')
-});
+// app.get('/', (req, res) => {
+//     console.log('This is a Fake Back-End Server');
+//     res.send('This is a Fake Back-End Server')
+// });
 
-app.post('/user', (req, res) => {
-    res.send(req.body);
-});
+// app.post('/user', (req, res) => {
+//     res.send(req.body);
+// });
 
-app.listen(5000, () => {
-    console.log('App Listening at port 5000')
-});
+// app.listen(5000, () => {
+//     console.log('App Listening at port 5000')
+// });
+
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
